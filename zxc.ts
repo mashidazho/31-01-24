00588c277eab2ec3e920819ae94041f6ce82a714
mashidazho <150504902+mashidazho@users.noreply.github.com>
2024-01-31 17:50:56 +1000

    Add files via upload

---
+++ zxc.ts
@@ -0,0 +1,21 @@
+class ClassName{}
+class Point{
+   x:number;
+   y:number
+}
+let point: Point = new Point()
+
+function Name() {
+    point.x=1
+    point.y=1
+}
+class Point2{
+    width: number;
+    height: number
+}
+let point2: Point2 = new Point2()
+
+function Name2() {
+    point2.width=3
+    point2.height=2
+}
